65d80f5f4179cf29231e3680ad480f69eb9b791b
Esed Tepeler <mesedtepeler25@gmail.com>
2025-08-06 14:16:09 +0300

    Backend yenilenmiş

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,11 @@
 # FastAPI uygulamasının başlangıç noktası
+# app/main.py
+
+from fastapi import FastAPI
+from backend.app.routers import health, upload
+
+app = FastAPI()
+
+# Register routes
+backend.app.include_router(health.router)
+backend.app.include_router(upload.router)
